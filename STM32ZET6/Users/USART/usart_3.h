2f839a0e67f95aa2847a0ee46054866364c81e57
DuRuofu <duruofu@qq.com>
2023-07-26 16:46:25 +0800

    添加 V1.0.0 分支

--- STM32ZET6/Users/USART/usart_3.h
+++ STM32ZET6/Users/USART/usart_3.h
@@ -8,7 +8,6 @@
 #ifndef __USART_3_H
 #define __USART_3_H
 
-
 //串口1接收中断初始化
 void  UART3_Init(void);
 //串口1接收完成回调函数
@@ -16,7 +15,3 @@ void UART3_RxCpltCallback(UART_HandleTypeDef *huart);
 void UART3_ErrorCallback(UART_HandleTypeDef *huart);
 //void Usart3_SendString(uint8_t *str);
 #endif // !__USART_3_H
-
-
-
-
